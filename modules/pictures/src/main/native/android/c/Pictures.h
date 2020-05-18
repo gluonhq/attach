87d800520a0f3995ebd8a5b223f11c6620dc48a1
jose.pereda <jose.pereda@gluonhq.com>
2020-05-18 13:30:08 +0200

    #108 Implement AndroidPicturesService

--- modules/pictures/src/main/native/android/c/Pictures.h
+++ modules/pictures/src/main/native/android/c/Pictures.h
@@ -25,10 +25,6 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package com.gluonhq.helloandroid;
 
-public interface LifecycleEventHandler {
-
-    void lifecycleEvent(String event);
-
-}
+#include "Util.h"
+extern jclass substrateGetPicturesServiceClass();
