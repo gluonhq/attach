a38ec1606cda7ba8f5fbc8f890c0ea87b54ccb8d
jose.pereda <jose.pereda@gluonhq.com>
2020-05-28 18:07:09 +0200

    rename onload method

--- modules/orientation/src/main/native/android/c/orientation.c
+++ modules/orientation/src/main/native/android/c/orientation.c
@@ -52,10 +52,10 @@ static void initializeOrientationDalvikHandles() {
 //////////////////////////
 
 JNIEXPORT jint JNICALL
-JNI_OnLoad_Orientation(JavaVM *vm, void *reserved)
+JNI_OnLoad_orientation(JavaVM *vm, void *reserved)
 {
     JNIEnv* graalEnv;
-    ATTACH_LOG_INFO("JNI_OnLoad_Orientation called\n");
+    ATTACH_LOG_INFO("JNI_OnLoad_orientation called");
 #ifdef JNI_VERSION_1_8
     if ((*vm)->GetEnv(vm, (void **)&graalEnv, JNI_VERSION_1_8) != JNI_OK) {
         ATTACH_LOG_WARNING("Error initializing native Orientation from OnLoad");
