ee1c62ba43e4f7db444002f17e3bd9a2595f2186
José Pereda <pereda@eii.uva.es>
2020-06-04 15:58:10 +0200

    Add Dialer implementation for Android (#150)
    * Add Dialer implementation for Android
    
    * Fix signature
    
    * Use CallVoidMethod
    
    * remove result
    
    Co-authored-by: jose.pereda <jose.pereda@gluonhq.com>

---
+++ modules/dialer/src/main/native/android/dalvik/DalvikDialerService.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2020, Gluon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL GLUON BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.gluonhq.helloandroid;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+
+public class DalvikDialerService {
+
+    private static final String TAG = Util.TAG;
+
+    private final Activity activity;
+    private final boolean debug;
+
+    public DalvikDialerService(Activity activity) {
+        this.activity = activity;
+        this.debug = Util.isDebug();
+    }
+
+    private void call(String number) {
+        boolean dialerEnabled = Util.verifyPermissions(Manifest.permission.CALL_PHONE);
+        if (!dialerEnabled) {
+            Log.e(TAG, "Dialer disabled");
+            return;
+        }
+
+        if (number == null || number.isEmpty()) {
+            Log.e(TAG, "Invalid number: number was null or empty");
+            return;
+        }
+        if (debug) {
+            Log.v(TAG, "Calling number: " + number);
+        }
+        Uri uriNumber = Uri.parse("tel:" + number);
+        Intent dialIntent = new Intent(Intent.ACTION_CALL, uriNumber);
+        dialIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        activity.startActivity(dialIntent);
+    }
+}
