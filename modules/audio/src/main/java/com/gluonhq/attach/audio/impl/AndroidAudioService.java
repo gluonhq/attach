6bb6874b68bbe779d79a97342a9453b592b42da2
Almas Baimagambetov <almaslvl@gmail.com>
2020-08-13 22:38:35 +0100

    refactor

--- modules/audio/src/main/java/com/gluonhq/attach/audio/impl/AndroidAudioService.java
+++ modules/audio/src/main/java/com/gluonhq/attach/audio/impl/AndroidAudioService.java
@@ -30,19 +30,22 @@ package com.gluonhq.attach.audio.impl;
 import com.gluonhq.attach.audio.Audio;
 import com.gluonhq.attach.audio.AudioService;
 import com.gluonhq.attach.storage.StorageService;
-import com.gluonhq.attach.util.Services;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.logging.Logger;
 
 /**
  * @author Almas Baimagambetov (almaslvl@gmail.com)
  */
 public class AndroidAudioService implements AudioService {
+
+    private static final Logger LOG = Logger.getLogger(AndroidAudioService.class.getName());
 
     static {
         System.loadLibrary("audio");
@@ -52,106 +55,70 @@ public class AndroidAudioService implements AudioService {
 
     @Override
     public Optional<Audio> loadSound(URL url) {
-        String fullName = prepareFile(url, "sounds/");
-
-        if (fullName.isEmpty())
-            return Optional.empty();
-
-        int audioId = loadSoundImpl(fullName);
-
-        if (audioId == -1)
-            return Optional.empty();
-
-        return Optional.of(new AndroidAudio(audioId));
+        return loadAudioImpl(url, AndroidAudioService::loadSoundImpl);
     }
 
     @Override
     public Optional<Audio> loadMusic(URL url) {
-        String fullName = prepareFile(url, "music/");
+        return loadAudioImpl(url, AndroidAudioService::loadMusicImpl);
+    }
 
-        if (fullName.isEmpty())
+    private Optional<Audio> loadAudioImpl(URL url, Function<String, Integer> loaderFunc) {
+        if (url == null)
             return Optional.empty();
 
-        int audioId = loadMusicImpl(fullName);
+        try {
+            String fullName = copyToPrivateStorageIfNeeded(url);
 
-        if (audioId == -1)
+            if (fullName.isEmpty())
+                return Optional.empty();
+
+            int audioId = loaderFunc.apply(fullName);
+
+            if (audioId == -1)
+                return Optional.empty();
+
+            return Optional.of(new AndroidAudio(audioId));
+
+        } catch (Exception e) {
+            LOG.fine("Error during loading audio: " + e);
             return Optional.empty();
-
-        return Optional.of(new AndroidAudio(audioId));
+        }
     }
 
     /**
      * Copy file (if it doesn't exist) to private storage.
      *
      * @param url where the file is
-     * @param subDirName subdirectory name where the file will go
      * @return file name in private storage if file was copied or if it already exists at destination,
-     * or empty String if there are any errors during copying
+     * @throws an exception if any error occurred during copying
      */
-    private String prepareFile(URL url, String subDirName) {
+    private String copyToPrivateStorageIfNeeded(URL url) throws Exception {
         if (privateStorage == null) {
-            privateStorage = setUpDirectories();
+            privateStorage = StorageService.create()
+                    .flatMap(StorageService::getPrivateStorage)
+                    .orElseThrow(() -> new RuntimeException("Error accessing Private Storage folder"));
         }
 
         String extForm = url.toExternalForm();
         String fileName = extForm.substring(extForm.lastIndexOf("/") + 1);
-        String fullName = privateStorage.getAbsolutePath() + "/assets/" + subDirName + fileName;
 
-        File outputFile = new File(fullName);
+        Path file = privateStorage.toPath()
+                .resolve("assets")
+                .resolve("audio")
+                .resolve(fileName);
+        
+        if (!Files.exists(file)) {
+            if (!Files.exists(file.getParent())) {
+                Files.createDirectories(file.getParent());
+            }
 
-        if (!outputFile.exists()) {
-            try {
-                copyFile(url, outputFile);
-            } catch (Exception e) {
-
-                // TODO:
-                e.printStackTrace();
-
-                return "";
+            try (InputStream input = url.openStream()) {
+                Files.copy(input, file);
             }
         }
 
-        return fullName;
-    }
-
-    private File setUpDirectories() {
-        File storage = Services.get(StorageService.class)
-                .flatMap(service -> service.getPrivateStorage())
-                .orElseThrow(() -> new RuntimeException("Error accessing Private Storage folder"));
-
-        File assetsDir = new File(storage, "assets");
-        if (!assetsDir.exists()) {
-            assetsDir.mkdir();
-        }
-
-        File musicDir = new File(assetsDir, "music");
-        if (!musicDir.exists()) {
-            musicDir.mkdir();
-        }
-
-        File soundsDir = new File(assetsDir, "sounds");
-        if (!soundsDir.exists()) {
-            soundsDir.mkdir();
-        }
-
-        return storage;
-    }
-
-    private void copyFile(URL url, File outputFile) throws Exception {
-        try (InputStream input = url.openStream()) {
-            if (input == null) {
-                throw new RuntimeException("Internal copy failed: input stream for " + url + " is null");
-            }
-
-            try (OutputStream output = new FileOutputStream(outputFile)) {
-                byte[] buffer = new byte[1024];
-                int length;
-                while ((length = input.read(buffer)) > 0) {
-                    output.write(buffer, 0, length);
-                }
-                output.flush();
-            }
-        }
+        return file.toAbsolutePath().toString();
     }
 
     private static class AndroidAudio implements Audio {
