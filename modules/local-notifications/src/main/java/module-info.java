58009f7d413d63c70775f10e0c6ef6ccc82884d4
Abhinay Agarwal <abhinay.agarwal@gluonhq.com>
2020-09-18 09:47:02 +0530

    Fix module names

--- modules/local-notifications/src/main/java/module-info.java
+++ modules/local-notifications/src/main/java/module-info.java
@@ -25,7 +25,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-module com.gluonhq.attach.local.notifications {
+module com.gluonhq.attach.localnotifications {
 
     requires javafx.graphics;
     requires com.gluonhq.attach.util;
