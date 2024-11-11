db59a545fa643067f81aff18bd4e2ce2cbe9da29
jose.pereda <jose.pereda@gluonhq.com>
2024-11-11 14:28:30 +0100

    Add new permissions to picture service

--- modules/pictures/src/main/native/android/dalvik/DalvikPicturesService.java
+++ modules/pictures/src/main/native/android/dalvik/DalvikPicturesService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, Gluon
+ * Copyright (c) 2020, 2024, Gluon
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -73,8 +73,16 @@ public class DalvikPicturesService  {
 
     private boolean verifyPermissions() {
         if (!verified) {
-            verified = Util.verifyPermissions(Manifest.permission.CAMERA,
-                    Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            if (Build.VERSION.SDK_INT >= 33) {
+                verified = Util.verifyPermissions(Manifest.permission.CAMERA,
+                        Manifest.permission.READ_MEDIA_AUDIO,
+                        Manifest.permission.READ_MEDIA_IMAGES,
+                        Manifest.permission.READ_MEDIA_VIDEO);
+            } else {
+                verified = Util.verifyPermissions(Manifest.permission.CAMERA,
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            }
         }
         return verified;
     }
